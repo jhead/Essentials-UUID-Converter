ceac0901f7d7cb9babd4e5390ab9a349c4e9b9e7
Justin Head <justin@cubedhost.com>
2015-04-04 11:55:43 -0500

    Fix multiple IPs, added UUID cache, added workers

--- convert.js
+++ convert.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
     async = require('async'),
     yaml = require('js-yaml');
 
+var cache = {};
 var options = {};
 var BASE_URL = "https://api.mojang.com/users/profiles/minecraft/";
 var addrIndex = 0;
@@ -10,29 +11,61 @@ var addrIndex = 0;
 var UserdataConverter = module.exports = function(opt) {
 
   options = opt;
-  console.log(options.addresses);
 
-  console.log('Fetching userdata file list from ' + opt.input + '/');
+  if (options.cache) {
+    console.log('Cache mode enabled. Press CTRL+C to quit and automatically save the cache.');
+    console.log('Progress should be saved for the next run.');
 
-  UserdataConverter.findUserdataFiles(opt.input + '/', function(err, files) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+    process.on('SIGINT', function() {
+      process.stdout.write('\n\nWriting UUID cache... ');
+      fs.writeFileSync('.uuidcache', new Buffer(JSON.stringify(cache)).toString('base64'));
+      process.stdout.write('Done!\n');
 
-    console.log('Found ' + files.length + ' files!');
-    UserdataConverter.convertFiles(files, function(err, converted) {
-      console.log('Successfully converted ' + converted.length + ' files!');
+      process.exit();
     });
 
-  });
+    try {
+      if (fs.existsSync('.uuidcache')) {
+        var encoded = fs.readFileSync('.uuidcache').toString('utf8'),
+            decoded = new Buffer(encoded, 'base64').toString('utf8');
 
+        cache = JSON.parse(decoded);
+        console.log('Successfuly loaded ' + Object.keys(cache).length + ' UUIDs from cache');
+      }
+    } catch (err) {
+      console.error('Failed to load .uuidcache file. Delete the file and try again.');
+      console.error(err);
+
+      process.exit(4);
+    }
+
+    console.log();
+  }
+
+  console.log('Conversion will begin in 5 seconds...\n');
+  setTimeout( function() {
+    console.log('Fetching userdata file list from ' + opt.input + '/');
+
+    UserdataConverter.findUserdataFiles(opt.input + '/', function(err, files) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log('Found ' + files.length + ' files!');
+      UserdataConverter.convertFiles(files, function(err, converted) {
+        console.log('Successfully converted ' + converted.length + ' files!');
+      });
+    });
+
+  }, 5000);
 };
 
 /**
- * [getFiles description]
- * @param {[type]}   dir      [description]
- * @param {Function} callback [description]
+ * Finds valid userdata yml filenames in specified directory
+ *
+ * @param {String}   dir      Directory to search
+ * @param {Function} callback (err, files)
  */
 UserdataConverter.findUserdataFiles = function(dir, callback) {
 
@@ -59,37 +92,44 @@ UserdataConverter.findUserdataFiles = function(dir, callback) {
 };
 
 /**
- * [bulkProcessFiles description]
- * @param {[type]}   fileList [description]
- * @param {Function} callback [description]
+ * Performs UserdataConverter.convertFile on input file list.
+ * A list of successful conversions is provided via the callback.
+ *
+ * @param {Array}    files    list of filenames
+ * @param {Function} callback (err, convertedFiles)
  */
  UserdataConverter.convertFiles = function(files, callback) {
 
   var converted = [];
 
   function iterator(file, cb) {
-    UserdataConverter.convertFile(file, function(err) {
+    UserdataConverter.convertFile(file, function(err, data) {
       if (err) {
         console.error('ERROR (' + file + '): ' + err);
-        files.push(file);
+        // files.push(file);
       } else {
         converted.push(file);
+        cache[file] = data;
       }
 
       return cb();
     });
   }
 
-  async.eachLimit(files, 5, iterator, function(err) {
+  async.eachLimit(files, options.workers || 5, iterator, function(err) {
     callback(err, converted);
   });
 
 };
 
 /**
- * [processFile description]
- * @param {[type]}   file     [description]
- * @param {Function} callback [description]
+ * Takes username from filename (username.yml), converts it to a UUID,
+ * loads the YAML contents of the file, inserts/updates 'lastAccountName',
+ * then saves the file in the output directory using the formatted UUID as
+ * the filename.
+ *
+ * @param {String}   file     Filename to convert
+ * @param {Function} callback (err)
  */
  UserdataConverter.convertFile = function(file, callback) {
   var username = file.replace(/\.yml$/, '').trim(),
@@ -104,8 +144,7 @@ UserdataConverter.findUserdataFiles = function(dir, callback) {
     console.log('Warning: ' + file + ' contains lastAccountName already (' + doc.lastAccountName + ')');
   }
 
-  // Fetch UUID from Mojang API
-  UserdataConverter.uuid(username, options.timestamp, function(err, data) {
+  function updateFile(err, data) {
     if (err) {
       return callback(err);
     }
@@ -117,17 +156,32 @@ UserdataConverter.findUserdataFiles = function(dir, callback) {
     fs.writeFileSync(options.output + '/' + uuid + '.yml', yaml.safeDump(doc));
 
     console.log('Converted ' + file + ': ' + uuid + '.yml');
-    callback();
-  });
+    callback(null, data);
+  }
+
+  // Fetch UUID from Mojang API
+  if (cache[file]) {
+    updateFile(null, cache[file]);
+    console.log('Got ' + file + ' from cache!');
+  } else {
+    UserdataConverter.uuid(username, options.timestamp, updateFile);
+  }
 };
 
 /**
- * [getUuid description]
- * @param {[type]}   username  [description]
- * @param {[type]}   timestamp [description]
- * @param {Function} callback  [description]
+ * Takes a username plus optional timestamp and converts it to a
+ * 32-bit formatted UUID via the Mojang API. This method supports
+ * "load balanced" API requests over multiple IP addresses.
+ *
+ * @param {String}   username  Username to lookup UUID for
+ * @param {Number}   timestamp Optional Unix timestamp for 'at' parameter
+ * @param {Function} callback  (err, { uuid, name })
  */
- UserdataConverter.uuid = function(username, timestamp, callback) {
+UserdataConverter.uuid = function(username, timestamp, callback) {
+  if (cache[username]) {
+
+  }
+
   var path = '/users/profiles/minecraft/' + username;
   var raw = '';
 
@@ -155,7 +209,9 @@ UserdataConverter.findUserdataFiles = function(dir, callback) {
     try {
       data = JSON.parse(raw);
 
-      if (!data.id) {
+      if (data.error) {
+        throw new Error(data.errorMessage.replace(/^Error:\s/, ''));
+      } else if (!data.id) {
         throw new Error('ID field missing');
       } else if (!data.id.match(/[a-zA-Z0-9]{32}/)) {
         throw new Error('Invalid UUID in response');
@@ -180,10 +236,13 @@ UserdataConverter.findUserdataFiles = function(dir, callback) {
     hostname: 'api.mojang.com',
     path: path,
     port: 443,
-    method: 'GET',
-    localAddress: options.addresses[addrIndex]
+    method: 'GET'
   };
-  addrIndex = (addrIndex < options.addresses.length - 1 ? addrIndex + 1 : 0);
+
+  if (options.addresses.length > 0) {
+    httpOptions.localAddress = options.addresses[addrIndex];
+    addrIndex = (addrIndex < options.addresses.length - 1 ? addrIndex + 1 : 0);
+  }
 
   var req = https.request(httpOptions, onResponse);
 
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ cmd
   .usage('[options] userdataDir outputDir')
   .option('-t, --timestamp [timestamp]', 'Fetch UUIDs at a specific (UTC) timestamp')
   .option('-i, --interface [iface]', 'Uses all IPs on specified interface to make API requests (to avoid rate limit)')
+  .option('-w --workers [number]', 'Number of workers to run simultaneously (default 5)')
+  .option('-c --cache', 'Create/load UUID cache file to allow resuming cancelled conversions')
   .option('-4', 'Only use IPv4')
   .parse(process.argv);
 
@@ -14,35 +16,51 @@ if (cmd.args.length !== 2) {
   cmd.help();
 }
 
+console.log('Essentials Userdata UUID Converter');
+console.log('Author: Justin Head (justin@cubedhost.com)');
+console.log();
+
+var workers = 5;
 var addresses = [];
 var timestamp = Math.round(new Date().getTime() / 1000);
 
+if (cmd.workers) {
+  workers = cmd.workers;
+}
+
 // 1422921600
 if (cmd.timestamp) {
+  console.log('Using timestamp ' + cmd.timestamp);
   timestamp = cmd.timestamp;
 }
 
 if (cmd.interface) {
-  var iface = os.networkInterfaces()[cmd.interface];
+  var ifaces = os.networkInterfaces();
+  var regex = new RegExp('^' + cmd.interface + '(:\\d+)?$');
 
-  if (!iface) {
-    console.error('Interface does not exist: ' + cmd.interface);
-    process.exit(2);
-  }
+  for (var ifaceName in ifaces) {
+    if (!ifaceName.match(regex)) continue;
 
-  for (var a in iface) {
-    var addr = iface[a].address;
-    if (!addr) continue;
+    var iface = ifaces[ifaceName];
+    for (var a in iface) {
+      var addr = iface[a].address;
+      if (!addr) continue;
 
-    if (cmd['4']) {
-      var matches = addr.match(/^(\d+)\.(\d+)\.(\d+)\.(\d)+$/);
-      if (!matches) continue;
+      if (cmd['4']) {
+        var matches = addr.match(/^(\d+)\.(\d+)\.(\d+)\.(\d)+$/);
+        if (!matches) continue;
+      }
+
+      addresses.push(addr);
     }
-
-    addresses.push(addr);
   }
 
-  console.log('Loaded ' + addresses.length + ' local address(es)');
+  if (addresses.length === 0) {
+    console.error('Failed to detect addresses on interface ' + cmd.interface);
+    process.exit(2);
+  } else {
+    console.log('Using ' + addresses.length + ' local address(es) from interface ' + cmd.interface);
+  }
 }
 
 if (!fs.existsSync(cmd.args[0])) {
@@ -54,9 +72,17 @@ if (!fs.existsSync(cmd.args[1])) {
   fs.mkdir(cmd.args[1]);
 }
 
+if (cmd.args[0] === cmd.args[1]) {
+  console.error('You cannot use the same input and output directories.');
+  process.exit(3);
+}
+
+console.log();
 require('./convert')({
   timestamp: timestamp,
   addresses: addresses,
   input: cmd.args[0],
-  output: cmd.args[1]
+  output: cmd.args[1],
+  workers: workers,
+  cache: cmd.cache
 });
